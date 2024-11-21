8460a3c040e2f2ce77329d4fe19768d6fde2178e
f7dec8 <f7dec8@gmail.com>
2024-11-21 23:55:44 +0900

    끄적끄적

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -9,21 +9,40 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      // { text: 'Examples', link: '/markdown-examples' }
     ],
 
     sidebar: [
       {
-        text: 'Examples',
+        text: '소식',
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
+          { text: '업데이트', link: '/소식/업데이트/' },
+          // { text: 'Runtime API Examples', link: '/api-examples' }
         ]
-      }
+      },
+      {
+        text: '출석부',
+        items: [
+          { text: '2회차', link: '/출석부/2회차' },
+          { text: '1회차', link: '/출석부/1회차' },
+        ]
+      },
+      {
+        text: '던전',
+        items: [
+          { text: '홍문가호', link: '/던전/홍문가호' },
+        ]
+      },
+      {
+        text: '교환',
+        items: [
+          { text: '전장', link: '/교환/전장' },
+        ]
+      },
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/f7dec8/bns-neo' }
     ]
   }
 })
